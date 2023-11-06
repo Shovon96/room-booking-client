8c4d955d67a8940ec3cce44190b073e76546a218
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-07 01:28:00 +0600

    bookings add to database

--- src/components/RoomDetails/RoomDetails.jsx
+++ src/components/RoomDetails/RoomDetails.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 
 const RoomDetails = () => {
@@ -9,9 +10,10 @@ const RoomDetails = () => {
     const { id } = useParams()
     const [roomDetails, setRoomDetails] = useState([]);
     const [selectedDate, setSelectedDate] = useState(null);
+    const {user} = useContext(AuthContext)
 
+    // datePicker
     const today = new Date();
-
     const handleDateChange = (date) => {
         setSelectedDate(date);
     };
@@ -21,7 +23,30 @@ const RoomDetails = () => {
             .then(res => res.json())
             .then(data => setRoomDetails(data))
     }, [id])
-    console.log(roomDetails);
+    // console.log(roomDetails);
+
+    const bookingsData = {
+        email: user.email,
+        image: roomDetails.image,
+        price: roomDetails.price,
+        date: roomDetails.date,
+        name: roomDetails.name
+    }
+
+    const handleBookings = () => {
+        fetch('http://localhost:5000/bookings', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(bookingsData)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                alert('Bookings added')
+            }
+        })
+    }
+
     return (
         <div className="max-w-6xl mx-auto mb-8 shadow-lg">
             <img className="w-full h-[90vh] rounded-md" src={roomDetails?.image} alt="" />
@@ -46,6 +71,9 @@ const RoomDetails = () => {
                 </div>
             </div>
             <h4 className="lg:px-24 px-5 py-3 text-lg text-gray-500 font-medium">{roomDetails?.description}</h4>
+            <div className="flex justify-center py-3">
+                <button onClick={handleBookings} className="btn btn-primary w-2/6">Book Now</button>
+            </div>
         </div>
     );
 };
