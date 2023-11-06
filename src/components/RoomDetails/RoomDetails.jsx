ce2a4c0734d7c4ff4683374dca505a40c9283d17
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-06 22:11:31 +0600

    roomDetails page added

--- src/components/RoomDetails/RoomDetails.jsx
+++ src/components/RoomDetails/RoomDetails.jsx
@@ -26,7 +26,7 @@ const RoomDetails = () => {
         <div className="max-w-6xl mx-auto mb-8 shadow-lg">
             <img className="w-full h-[90vh] rounded-md" src={roomDetails?.image} alt="" />
 
-            <div className="flex justify-evenly my-6">
+            <div className="md:flex md:justify-evenly ml-32 md:ml-0 my-3 lg:my-6">
                 <div>
                     <h2 className="text-blue-500 text-2xl my-2 font-bold">{roomDetails?.name}</h2>
                     <h4 className="text-lg font-semibold my-2">Price: ${roomDetails?.price} <span className="text-sm text-gray-500">perday</span></h4>
@@ -45,14 +45,7 @@ const RoomDetails = () => {
                     }</h3>
                 </div>
             </div>
-            
-            {/* <div className="flex justify-evenly text-xl font-bold mt-3">
-            </div>
-            <div className="flex justify-evenly text-lg font-bold mt-3">
-            </div>
-            <div className="flex justify-evenly text-lg font-bold mt-3 pb-3">
-
-            </div> */}
+            <h4 className="lg:px-24 px-5 py-3 text-lg text-gray-500 font-medium">{roomDetails?.description}</h4>
         </div>
     );
 };
