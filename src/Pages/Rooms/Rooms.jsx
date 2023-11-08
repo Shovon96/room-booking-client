0620fc142d739f25cbc04c3b8acee26948a3cd85
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-08 16:17:16 +0600

    sort by price added

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../../components/AuthProvider/AuthProvider";
 
 const Rooms = () => {
 
-    const [rooms, setRooms] = useState([])
+    const [rooms, setRooms] = useState([]);
     const {loading} = useContext(AuthContext)
 
     useEffect(() => {
@@ -12,6 +12,17 @@ const Rooms = () => {
             .then(res => res.json())
             .then(data => setRooms(data))
     }, [])
+
+    // useEffect(()=> {
+    //     fetch('http://localhost:5000/sort?sort=true')
+    //     .then(res => res.json())
+    // }, [])
+
+    const handleSortPrice = () => {
+        fetch('http://localhost:5000/sort?sort=true')
+        .then(res => res.json())
+        .then(data => setRooms(data))
+    }
 
     if(loading){
         return <div className="flex justify-center items-center h-[80vh]"><span className="loading loading-lg loading-spinner text-primary"></span></div>
@@ -23,6 +34,9 @@ const Rooms = () => {
                 <h1 className="text-5xl font-bold">Hotel Room Gallery</h1>
                 <h3 className="text-2xl font-semibold my-2">Welcome to Hotel Unparalleled Comfort and Elegance!</h3>
                 <p className="lg:px-56 mb-6">Indulge in the epitome of luxury with our spacious and meticulously designed deluxe suites. Enjoy breathtaking views, modern furnishings, and personalized service.Elevate your stay with our exclusive special offers. From romantic getaways to family packages, we have tailored options to suit every traveler.</p>
+            </div>
+            <div className="flex justify-end mb-4">
+                <button onClick={handleSortPrice} className="btn btn-primary">Sort By Price</button>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
                 {
